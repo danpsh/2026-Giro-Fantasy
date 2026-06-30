59c4d87e80aa7609d334a512ba02f38b55df60c7
danpsh <danielpshonyak@gmail.com>
2026-06-30 12:09:48 -0500

    Add files via upload

--- tdf-startlist.js
+++ tdf-startlist.js
@@ -1,1 +1,523 @@
-window.TDF_STARTLIST = {"race":"Tour de France","year":2026,"riders":[{"name":"Tadej Pogacar","team":"UAE Team Emirates - XRG"},{"name":"Isaac del Toro","team":"UAE Team Emirates - XRG"},{"name":"Tim Wellens","team":"UAE Team Emirates - XRG"},{"name":"Brandon McNulty","team":"UAE Team Emirates - XRG"},{"name":"Adam Yates","team":"UAE Team Emirates - XRG"},{"name":"Florian Vermeersch","team":"UAE Team Emirates - XRG"},{"name":"Nils Politt","team":"UAE Team Emirates - XRG"},{"name":"Felix Grossschartner","team":"UAE Team Emirates - XRG"},{"name":"Antonio Tiberi","team":"Bahrain - Victorious"},{"name":"Lenny Martinez","team":"Bahrain - Victorious"},{"name":"Matej Mohoric","team":"Bahrain - Victorious"},{"name":"Phil Bauhaus","team":"Bahrain - Victorious"},{"name":"Damiano Caruso","team":"Bahrain - Victorious"},{"name":"Kamil Gradek","team":"Bahrain - Victorious"},{"name":"Robert Stannard","team":"Bahrain - Victorious"},{"name":"Vlad van Mechelen","team":"Bahrain - Victorious"},{"name":"Paul Seixas","team":"Decathlon CMA CGM Team"},{"name":"Olav Kooij","team":"Decathlon CMA CGM Team"},{"name":"Matthew Riccitello","team":"Decathlon CMA CGM Team"},{"name":"Tiesj Benoot","team":"Decathlon CMA CGM Team"},{"name":"Daan Hoole","team":"Decathlon CMA CGM Team"},{"name":"Nicolas Prodhomme","team":"Decathlon CMA CGM Team"},{"name":"Aurelien Paret-Peintre","team":"Decathlon CMA CGM Team"},{"name":"Cees Bol","team":"Decathlon CMA CGM Team"},{"name":"Arnaud De Lie","team":"Lotto Intermarche"},{"name":"Lennert van Eetvelt","team":"Lotto Intermarche"},{"name":"Georg Zimmermann","team":"Lotto Intermarche"},{"name":"Huub Artz","team":"Lotto Intermarche"},{"name":"Jenno Berckmoes","team":"Lotto Intermarche"},{"name":"Liam Slock","team":"Lotto Intermarche"},{"name":"Lars Craps","team":"Lotto Intermarche"},{"name":"Baptiste Veistroffer","team":"Lotto Intermarche"},{"name":"Biniam Girmay","team":"NSN Cycling Team"},{"name":"Jake Stewart","team":"NSN Cycling Team"},{"name":"Lewis Askey","team":"NSN Cycling Team"},{"name":"Krists Neilands","team":"NSN Cycling Team"},{"name":"Marco Frigo","team":"NSN Cycling Team"},{"name":"Matis Louvel","team":"NSN Cycling Team"},{"name":"George Bennett","team":"NSN Cycling Team"},{"name":"Tom Van Asbroeck","team":"NSN Cycling Team"},{"name":"Tim van Dijke","team":"Red Bull - BORA - hansgrohe"},{"name":"Remco Evenepoel","team":"Red Bull - BORA - hansgrohe"},{"name":"Nico Denz","team":"Red Bull - BORA - hansgrohe"},{"name":"Florian Lipowitz","team":"Red Bull - BORA - hansgrohe"},{"name":"Mattia Cattaneo","team":"Red Bull - BORA - hansgrohe"},{"name":"Jan Tratnik","team":"Red Bull - BORA - hansgrohe"},{"name":"Maxim Van Gils","team":"Red Bull - BORA - hansgrohe"},{"name":"Jai Hindley","team":"Red Bull - BORA - hansgrohe"},{"name":"Valentin Paret-Peintre","team":"Soudal Quick-Step"},{"name":"Mikel Landa","team":"Soudal Quick-Step"},{"name":"Tim Merlier","team":"Soudal Quick-Step"},{"name":"Jasper Stuyven","team":"Soudal Quick-Step"},{"name":"Ilan Van Wilder","team":"Soudal Quick-Step"},{"name":"Louis Vervaeke","team":"Soudal Quick-Step"},{"name":"Dylan Van Baarle","team":"Soudal Quick-Step"},{"name":"Bert Van Lerberghe","team":"Soudal Quick-Step"},{"name":"Michael Matthews","team":"Team Jayco AlUla"},{"name":"Luke Plapp","team":"Team Jayco AlUla"},{"name":"Pascal Ackermann","team":"Team Jayco AlUla"},{"name":"Ben O'Connor","team":"Team Jayco AlUla"},{"name":"Mauro Schmid","team":"Team Jayco AlUla"},{"name":"Kelland O'Brien","team":"Team Jayco AlUla"},{"name":"Felix Engelhardt","team":"Team Jayco AlUla"},{"name":"Luke Durbridge","team":"Team Jayco AlUla"},{"name":"Pavel Bittner","team":"Team Picnic PostNL"},{"name":"Warren Barguil","team":"Team Picnic PostNL"},{"name":"Frank Van den Broek","team":"Team Picnic PostNL"},{"name":"Robbe Dhondt","team":"Team Picnic PostNL"},{"name":"Julius Van den Berg","team":"Team Picnic PostNL"},{"name":"Niklas Markl","team":"Team Picnic PostNL"},{"name":"Frits Biesterbos","team":"Team Picnic PostNL"},{"name":"John Degenkolb","team":"Team Picnic PostNL"},{"name":"Jonas Vingegaard","team":"Team Visma | Lease a Bike"},{"name":"Matteo Jorgenson","team":"Team Visma | Lease a Bike"},{"name":"Sepp Kuss","team":"Team Visma | Lease a Bike"},{"name":"Victor Campenaerts","team":"Team Visma | Lease a Bike"},{"name":"Edoardo Affini","team":"Team Visma | Lease a Bike"},{"name":"Per Strand Hagenes","team":"Team Visma | Lease a Bike"},{"name":"Bruno Armirail","team":"Team Visma | Lease a Bike"},{"name":"Davide Piganzoli","team":"Team Visma | Lease a Bike"},{"name":"Magnus Cort","team":"Uno-X Mobility"},{"name":"Tobias Halland Johannessen","team":"Uno-X Mobility"},{"name":"Anders Skaarseth","team":"Uno-X Mobility"},{"name":"Soren Waerenskjold","team":"Uno-X Mobility"},{"name":"Anthon Charmig","team":"Uno-X Mobility"},{"name":"Jonas Abrahamsen","team":"Uno-X Mobility"},{"name":"Torstein Traeen","team":"Uno-X Mobility"},{"name":"Anders Halland Johannessen","team":"Uno-X Mobility"},{"name":"Mike Teunissen","team":"XDS Astana Team"},{"name":"Sergio Higuita","team":"XDS Astana Team"},{"name":"Harold Tejada","team":"XDS Astana Team"},{"name":"Max Kanter","team":"XDS Astana Team"},{"name":"Nicolas Vinokurov","team":"XDS Astana Team"},{"name":"Davide Ballerini","team":"XDS Astana Team"},{"name":"Aaron Gate","team":"XDS Astana Team"},{"name":"Simone Velasco","team":"XDS Astana Team"},{"name":"Piet Allegaert","team":"Cofidis"},{"name":"Jenthe Biermans","team":"Cofidis"},{"name":"Milan Fretin","team":"Cofidis"},{"name":"Alex Kirsch","team":"Cofidis"},{"name":"Hugo Page","team":"Cofidis"},{"name":"Alex Aranburu","team":"Cofidis"},{"name":"Benjamin Thomas","team":"Cofidis"},{"name":"Ion Izagirre","team":"Cofidis"},{"name":"Julian Alaphilippe","team":"Tudor Pro Cycling Team"},{"name":"Matteo Trentin","team":"Tudor Pro Cycling Team"},{"name":"Michael Storer","team":"Tudor Pro Cycling Team"},{"name":"Rick Pluimers","team":"Tudor Pro Cycling Team"},{"name":"Arvid de Kleijn","team":"Tudor Pro Cycling Team"},{"name":"Marco Haller","team":"Tudor Pro Cycling Team"},{"name":"Marc Hirschi","team":"Tudor Pro Cycling Team"},{"name":"Yannis Voisard","team":"Tudor Pro Cycling Team"},{"name":"Jordan Jegat","team":"TotalEnergies"},{"name":"Alexandre Delettre","team":"TotalEnergies"},{"name":"Anthony Turgis","team":"TotalEnergies"},{"name":"Matteo Vercher","team":"TotalEnergies"},{"name":"Mathis Le Berre","team":"TotalEnergies"},{"name":"Nicolas Breuillard","team":"TotalEnergies"},{"name":"Joris Delbove","team":"TotalEnergies"},{"name":"Thibault Guernalec","team":"TotalEnergies"},{"name":"Alex Molenaar","team":"Caja Rural - Seguros RGA"},{"name":"Joel Nicolau","team":"Caja Rural - Seguros RGA"},{"name":"Abel Balderstone","team":"Caja Rural - Seguros RGA"},{"name":"Sebastian Berwick","team":"Caja Rural - Seguros RGA"},{"name":"Fernando Gaviria","team":"Caja Rural - Seguros RGA"},{"name":"Stefano Oldani","team":"Caja Rural - Seguros RGA"},{"name":"Jakub Otruba","team":"Caja Rural - Seguros RGA"},{"name":"Jose Felix Parra","team":"Caja Rural - Seguros RGA"},{"name":"Filippo Ganna","team":"Netcompany INEOS"},{"name":"Carlos Rodriguez","team":"Netcompany INEOS"},{"name":"Michal Kwiatkowski","team":"Netcompany INEOS"},{"name":"Kevin Vauquelin","team":"Netcompany INEOS"},{"name":"Dorian Godon","team":"Netcompany INEOS"},{"name":"Thymen Arensman","team":"Netcompany INEOS"},{"name":"Juan Ayuso","team":"Lidl - Trek"},{"name":"Mads Pedersen","team":"Lidl - Trek"},{"name":"Mathias Vacek","team":"Lidl - Trek"},{"name":"Mattias Skjelmose","team":"Lidl - Trek"},{"name":"Quinn Simmons","team":"Lidl - Trek"},{"name":"Soren Kragh Andersen","team":"Lidl - Trek"},{"name":"Jasper Philipsen","team":"Alpecin - Premier Tech"},{"name":"Mathieu van der Poel","team":"Alpecin - Premier Tech"},{"name":"Emiel Verstrynge","team":"Alpecin - Premier Tech"},{"name":"Jonas Rickaert","team":"Alpecin - Premier Tech"},{"name":"Ben Healy","team":"EF Education - EasyPost"},{"name":"Kasper Asgreen","team":"EF Education - EasyPost"},{"name":"Richard Carapaz","team":"EF Education - EasyPost"},{"name":"Alex Baudin","team":"EF Education - EasyPost"},{"name":"Cian Uijtdebroeks","team":"Movistar Team"},{"name":"Einer Rubio","team":"Movistar Team"},{"name":"Pablo Castrillo","team":"Movistar Team"},{"name":"Roger Adria","team":"Movistar Team"},{"name":"Tom Pidcock","team":"Pinarello Q36.5"},{"name":"Fred Wright","team":"Pinarello Q36.5"},{"name":"Quinten Hermans","team":"Pinarello Q36.5"},{"name":"Guillaume Martin","team":"Groupama - FDJ United"},{"name":"Romain Gregoire","team":"Groupama - FDJ United"}]};
+window.TDF_STARTLIST = {
+  race: "Tour de France",
+  year: 2026,
+  updated: "2026-06-30",
+  riders: [
+  {
+    "name": "Tadej Pogačar",
+    "team": "UAE Team Emirates-XRG"
+  },
+  {
+    "name": "Isaac Del Toro",
+    "team": "UAE Team Emirates-XRG"
+  },
+  {
+    "name": "Tim Wellens",
+    "team": "UAE Team Emirates-XRG"
+  },
+  {
+    "name": "Brandon McNulty",
+    "team": "UAE Team Emirates-XRG"
+  },
+  {
+    "name": "Adam Yates",
+    "team": "UAE Team Emirates-XRG"
+  },
+  {
+    "name": "Florian Vermeersch",
+    "team": "UAE Team Emirates-XRG"
+  },
+  {
+    "name": "Nils Politt",
+    "team": "UAE Team Emirates-XRG"
+  },
+  {
+    "name": "Jonas Vingegaard",
+    "team": "Visma | Lease a Bike"
+  },
+  {
+    "name": "Victor Campenaerts",
+    "team": "Visma | Lease a Bike"
+  },
+  {
+    "name": "Edoardo Affini",
+    "team": "Visma | Lease a Bike"
+  },
+  {
+    "name": "Per Strand Hagenes",
+    "team": "Visma | Lease a Bike"
+  },
+  {
+    "name": "Matteo Jorgenson",
+    "team": "Visma | Lease a Bike"
+  },
+  {
+    "name": "Sepp Kuss",
+    "team": "Visma | Lease a Bike"
+  },
+  {
+    "name": "Bruno Armirail",
+    "team": "Visma | Lease a Bike"
+  },
+  {
+    "name": "Davide Piganzoli",
+    "team": "Visma | Lease a Bike"
+  },
+  {
+    "name": "Remco Evenepoel",
+    "team": "Red Bull - BORA - hansgrohe"
+  },
+  {
+    "name": "Florian Lipowitz",
+    "team": "Red Bull - BORA - hansgrohe"
+  },
+  {
+    "name": "Jai Hindley",
+    "team": "Red Bull - BORA - hansgrohe"
+  },
+  {
+    "name": "Tim Van Dijke",
+    "team": "Red Bull - BORA - hansgrohe"
+  },
+  {
+    "name": "Nico Denz",
+    "team": "Red Bull - BORA - hansgrohe"
+  },
+  {
+    "name": "Mattia Cattaneo",
+    "team": "Red Bull - BORA - hansgrohe"
+  },
+  {
+    "name": "Jan Tratnik",
+    "team": "Red Bull - BORA - hansgrohe"
+  },
+  {
+    "name": "Maxim Van Gils",
+    "team": "Red Bull - BORA - hansgrohe"
+  },
+  {
+    "name": "Jasper Philipsen",
+    "team": "Alpecin-Premier Tech"
+  },
+  {
+    "name": "Mathieu Van Der Poel",
+    "team": "Alpecin-Premier Tech"
+  },
+  {
+    "name": "Kaden Groves",
+    "team": "Alpecin-Premier Tech"
+  },
+  {
+    "name": "Emiel Verstrynge",
+    "team": "Alpecin-Premier Tech"
+  },
+  {
+    "name": "Jonas Rickaert",
+    "team": "Alpecin-Premier Tech"
+  },
+  {
+    "name": "Antonio Tiberi",
+    "team": "Bahrain Victorious"
+  },
+  {
+    "name": "Lenny Martinez",
+    "team": "Bahrain Victorious"
+  },
+  {
+    "name": "Matej Mohoric",
+    "team": "Bahrain Victorious"
+  },
+  {
+    "name": "Phil Bauhaus",
+    "team": "Bahrain Victorious"
+  },
+  {
+    "name": "Damiano Caruso",
+    "team": "Bahrain Victorious"
+  },
+  {
+    "name": "Kamil Gradek",
+    "team": "Bahrain Victorious"
+  },
+  {
+    "name": "Robert Stannard",
+    "team": "Bahrain Victorious"
+  },
+  {
+    "name": "Vlad Van Mechelen",
+    "team": "Bahrain Victorious"
+  },
+  {
+    "name": "Paul Seixas",
+    "team": "Decathlon CMA CGM"
+  },
+  {
+    "name": "Olav Kooij",
+    "team": "Decathlon CMA CGM"
+  },
+  {
+    "name": "Tiesj Benoot",
+    "team": "Decathlon CMA CGM"
+  },
+  {
+    "name": "Daan Hoole",
+    "team": "Decathlon CMA CGM"
+  },
+  {
+    "name": "Stefan Bissegger",
+    "team": "Decathlon CMA CGM"
+  },
+  {
+    "name": "Ben Healy",
+    "team": "EF Education-EasyPost"
+  },
+  {
+    "name": "Richard Carapaz",
+    "team": "EF Education-EasyPost"
+  },
+  {
+    "name": "Kasper Asgreen",
+    "team": "EF Education-EasyPost"
+  },
+  {
+    "name": "Alex Baudin",
+    "team": "EF Education-EasyPost"
+  },
+  {
+    "name": "David Gaudu",
+    "team": "Groupama-FDJ United"
+  },
+  {
+    "name": "Guillaume Martin",
+    "team": "Groupama-FDJ United"
+  },
+  {
+    "name": "Romain Grégoire",
+    "team": "Groupama-FDJ United"
+  },
+  {
+    "name": "Filippo Ganna",
+    "team": "Netcompany INEOS"
+  },
+  {
+    "name": "Carlos Rodríguez",
+    "team": "Netcompany INEOS"
+  },
+  {
+    "name": "Kévin Vauquelin",
+    "team": "Netcompany INEOS"
+  },
+  {
+    "name": "Thymen Arensman",
+    "team": "Netcompany INEOS"
+  },
+  {
+    "name": "Michał Kwiatkowski",
+    "team": "Netcompany INEOS"
+  },
+  {
+    "name": "Dorian Godon",
+    "team": "Netcompany INEOS"
+  },
+  {
+    "name": "Juan Ayuso",
+    "team": "Lidl-Trek"
+  },
+  {
+    "name": "Mads Pedersen",
+    "team": "Lidl-Trek"
+  },
+  {
+    "name": "Mattias Skjelmose",
+    "team": "Lidl-Trek"
+  },
+  {
+    "name": "Giulio Ciccone",
+    "team": "Lidl-Trek"
+  },
+  {
+    "name": "Mathias Vacek",
+    "team": "Lidl-Trek"
+  },
+  {
+    "name": "Quinn Simmons",
+    "team": "Lidl-Trek"
+  },
+  {
+    "name": "Søren Kragh Andersen",
+    "team": "Lidl-Trek"
+  },
+  {
+    "name": "Arnaud De Lie",
+    "team": "Lotto Intermarché"
+  },
+  {
+    "name": "Lennert Van Eetvelt",
+    "team": "Lotto Intermarché"
+  },
+  {
+    "name": "Georg Zimmermann",
+    "team": "Lotto Intermarché"
+  },
+  {
+    "name": "Huub Artz",
+    "team": "Lotto Intermarché"
+  },
+  {
+    "name": "Cian Uijtdebroeks",
+    "team": "Movistar Team"
+  },
+  {
+    "name": "Einer Rubio",
+    "team": "Movistar Team"
+  },
+  {
+    "name": "Pablo Castrillo",
+    "team": "Movistar Team"
+  },
+  {
+    "name": "Roger Adrià",
+    "team": "Movistar Team"
+  },
+  {
+    "name": "Biniam Girmay",
+    "team": "NSN Cycling Team"
+  },
+  {
+    "name": "Jake Stewart",
+    "team": "NSN Cycling Team"
+  },
+  {
+    "name": "Lewis Askey",
+    "team": "NSN Cycling Team"
+  },
+  {
+    "name": "Krists Neilands",
+    "team": "NSN Cycling Team"
+  },
+  {
+    "name": "Marco Frigo",
+    "team": "NSN Cycling Team"
+  },
+  {
+    "name": "Matis Louvel",
+    "team": "NSN Cycling Team"
+  },
+  {
+    "name": "George Bennett",
+    "team": "NSN Cycling Team"
+  },
+  {
+    "name": "Tom Van Asbroeck",
+    "team": "NSN Cycling Team"
+  },
+  {
+    "name": "Tim Merlier",
+    "team": "Soudal Quick-Step"
+  },
+  {
+    "name": "Mikel Landa",
+    "team": "Soudal Quick-Step"
+  },
+  {
+    "name": "Valentin Paret-Peintre",
+    "team": "Soudal Quick-Step"
+  },
+  {
+    "name": "Jasper Stuyven",
+    "team": "Soudal Quick-Step"
+  },
+  {
+    "name": "Ilan Van Wilder",
+    "team": "Soudal Quick-Step"
+  },
+  {
+    "name": "Louis Vervaeke",
+    "team": "Soudal Quick-Step"
+  },
+  {
+    "name": "Dylan Van Baarle",
+    "team": "Soudal Quick-Step"
+  },
+  {
+    "name": "Bert Van Lerberghe",
+    "team": "Soudal Quick-Step"
+  },
+  {
+    "name": "Ben O'Connor",
+    "team": "Jayco AlUla"
+  },
+  {
+    "name": "Michael Matthews",
+    "team": "Jayco AlUla"
+  },
+  {
+    "name": "Luke Plapp",
+    "team": "Jayco AlUla"
+  },
+  {
+    "name": "Pascal Ackermann",
+    "team": "Jayco AlUla"
+  },
+  {
+    "name": "Mauro Schmid",
+    "team": "Jayco AlUla"
+  },
+  {
+    "name": "Kelland O'Brien",
+    "team": "Jayco AlUla"
+  },
+  {
+    "name": "Felix Engelhardt",
+    "team": "Jayco AlUla"
+  },
+  {
+    "name": "Luke Durbridge",
+    "team": "Jayco AlUla"
+  },
+  {
+    "name": "Pavel Bittner",
+    "team": "Picnic PostNL"
+  },
+  {
+    "name": "Warren Barguil",
+    "team": "Picnic PostNL"
+  },
+  {
+    "name": "Frank Van Den Broek",
+    "team": "Picnic PostNL"
+  },
+  {
+    "name": "Tobias Halland Johannessen",
+    "team": "Uno-X Mobility"
+  },
+  {
+    "name": "Magnus Cort Nielsen",
+    "team": "Uno-X Mobility"
+  },
+  {
+    "name": "Søren Wærenskjold",
+    "team": "Uno-X Mobility"
+  },
+  {
+    "name": "Anders Skaarseth",
+    "team": "Uno-X Mobility"
+  },
+  {
+    "name": "Anthon Charmig",
+    "team": "Uno-X Mobility"
+  },
+  {
+    "name": "Jonas Abrahamsen",
+    "team": "Uno-X Mobility"
+  },
+  {
+    "name": "Torstein Træen",
+    "team": "Uno-X Mobility"
+  },
+  {
+    "name": "Anders Halland Johannessen",
+    "team": "Uno-X Mobility"
+  },
+  {
+    "name": "Sergio Higuita",
+    "team": "XDS Astana"
+  },
+  {
+    "name": "Mike Teunissen",
+    "team": "XDS Astana"
+  },
+  {
+    "name": "Lorenzo Fortunato",
+    "team": "XDS Astana"
+  },
+  {
+    "name": "Harold Tejada",
+    "team": "XDS Astana"
+  },
+  {
+    "name": "Max Kanter",
+    "team": "XDS Astana"
+  },
+  {
+    "name": "Nicolas Vinokurov",
+    "team": "XDS Astana"
+  },
+  {
+    "name": "Ion Izagirre",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Alex Aranburu",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Emanuel Buchmann",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Alex Kirsch",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Hugo Page",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Milan Fretin",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Piet Allegaert",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Jenthe Biermans",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Benjamin Thomas",
+    "team": "Cofidis"
+  },
+  {
+    "name": "Julian Alaphilippe",
+    "team": "Tudor Pro Cycling"
+  },
+  {
+    "name": "Michael Storer",
+    "team": "Tudor Pro Cycling"
+  },
+  {
+    "name": "Matteo Trentin",
+    "team": "Tudor Pro Cycling"
+  },
+  {
+    "name": "Rick Pluimers",
+    "team": "Tudor Pro Cycling"
+  },
+  {
+    "name": "Jordan Jegat",
+    "team": "TotalEnergies"
+  },
+  {
+    "name": "Alex Molenaar",
+    "team": "Caja Rural"
+  },
+  {
+    "name": "Joel Nicolau",
+    "team": "Caja Rural"
+  },
+  {
+    "name": "Tom Pidcock",
+    "team": "Pinarello Q36.5"
+  },
+  {
+    "name": "Fred Wright",
+    "team": "Pinarello Q36.5"
+  },
+  {
+    "name": "Quinten Hermans",
+    "team": "Pinarello Q36.5"
+  }
+]
+};
